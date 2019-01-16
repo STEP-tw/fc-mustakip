9de7669cab6f85ddbaf09ef071f67ecee780c79d
mustakim patvekar <44019216+mustakip@users.noreply.github.com>
2019-01-16 13:26:05 +0530

    made a html page
    the nex objective is to make the functionality asyncronous

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+const html = fs.readFileSync('index.html', 'utf8');
+const cssFile = fs.readFileSync('style.css', 'utf8');
+
 const app = (req, res) => {
-  res.statusCode = 404;
+  res.write(html);
   res.end();
 };
 
